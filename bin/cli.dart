51ce42a9761686a264cdc9afdad977cc576764bc
Suebin Kim <k.sue3028@gmail.com>
2024-01-09 16:47:23 +0900

    feat: check Object

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,6 +1,11 @@
 import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
-  int? variable = null;
-  print(variable);
+  var a = "2"; // no type specification
+  String b = "3";
+  Object c = 2;
+  print(c);
+
+  // int variable = null;
+  // print(variable);
 }
