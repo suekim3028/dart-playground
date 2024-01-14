c6ee60893790213b19ee258f81cb8ebbd131926a
Suebin Kim <k.sue3028@gmail.com>
2024-01-14 21:09:56 +0900

    feat: practice set

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,22 +1,6 @@
 import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
-  List<String> a = ["a", "b", "c"];
-  print(a);
-  print(a.length);
-  a.add("d");
-  print(a);
-  a.remove("d");
-  a.indexOf("b");
-  print(a.map((b) => b + "a"));
-
-  Map<String, int> dict = {"a": 2, "b": 2, "c": 4};
-  print(dict);
-
-  dict.addAll({"2": 3, "c": 2});
-  dict.remove("c");
-  print(dict);
-
-  print(dict.keys);
-  print(dict.values);
+  final Set<String> names = {"a", "a", "b"};
+  print(names);
 }
