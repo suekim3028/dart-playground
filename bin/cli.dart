c104286f4df6b2ed19ddad73530bfa0abad3e490
Suebin Kim <k.sue3028@gmail.com>
2024-01-14 21:45:29 +0900

    feat: practice named params

--- bin/cli.dart
+++ bin/cli.dart
@@ -3,10 +3,13 @@ import 'package:cli/cli.dart' as cli;
 enum Status { approved, pending, rejected }
 
 void main(List<String> arguments) {
-  int a = addNumbers(1, 2);
+  int a = addNumbers(y: 1, x: 2);
   print(a);
 }
 
-int addNumbers(int a, int b, [int c = 10, int d = 30]) {
-  return a + b + c;
+int addNumbers({
+  required int x,
+  required int y,
+}) {
+  return x + y;
 }
