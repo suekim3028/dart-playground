206959465638f8ebf7ce61261a705aa669837fac
Suebin Kim <k.sue3028@gmail.com>
2024-01-14 21:52:41 +0900

    feat: practice typedef

--- bin/cli.dart
+++ bin/cli.dart
@@ -3,13 +3,10 @@ import 'package:cli/cli.dart' as cli;
 enum Status { approved, pending, rejected }
 
 void main(List<String> arguments) {
-  int a = addNumbers(y: 1, x: 2, w: 3);
-  print(a);
+  Operation _a = a;
+  int b = _a(2, 3, 4);
 }
 
-int addNumbers(int a, int b,
-    {required int x, required int y, int z = 20, int? w}) {
-  return x + y + z + (w ?? 20);
-}
+typedef Operation = int Function(int x, int y, int z);
 
-int addNumbers2(int a, int b) => a + b;
+int a(int x, int y, int z) => x + y + z;
