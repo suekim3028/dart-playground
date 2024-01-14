870103d8b0c42f84506fd877ba9ec665700f080a
Suebin Kim <k.sue3028@gmail.com>
2024-01-14 21:21:27 +0900

    feat: practice switch

--- bin/cli.dart
+++ bin/cli.dart
@@ -2,5 +2,17 @@ import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
   final Set<String> names = {"a", "a", "b"};
-  print(names);
+  // print(names);
+
+  String name = "a";
+  switch (name) {
+    case "b":
+      print("a");
+      break;
+    case "a":
+      print("b");
+      break;
+    default:
+      print("c");
+  }
 }
