0e53db22d56a1b1c17319354de9596599fd6fb51
Suebin Kim <k.sue3028@gmail.com>
2024-01-14 21:46:15 +0900

    feat: practice named params with optional

--- bin/cli.dart
+++ bin/cli.dart
@@ -3,13 +3,10 @@ import 'package:cli/cli.dart' as cli;
 enum Status { approved, pending, rejected }
 
 void main(List<String> arguments) {
-  int a = addNumbers(y: 1, x: 2);
+  int a = addNumbers(y: 1, x: 2, w: 3);
   print(a);
 }
 
-int addNumbers({
-  required int x,
-  required int y,
-}) {
-  return x + y;
+int addNumbers({required int x, required int y, int z = 20, int? w}) {
+  return x + y + z + (w ?? 20);
 }
